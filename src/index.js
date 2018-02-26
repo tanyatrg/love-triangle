202e1113a5531e86171dbcd1bb32b7e4344be4bd
Your Name <tanusha_troyanova@mail.ru>
2018-02-26 13:06:03 +0300

    My commit!

--- src/index.js
+++ src/index.js
@@ -4,4 +4,25 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+  var count = 0;
+  var A = 0;
+  var B = 0;
+  var C = 0;
+  var D = 0;
+  var otvet = 0;
+  for (var i = 0; i < preferences.length; i++) {
+    if(i!=A-1 && i!=B-1 && i!=C-1 ){
+      A = preferences[i];
+      B = preferences[A-1];
+      C = preferences[B-1];
+      D = preferences[C-1];
+      if (A != B && B != C && A != C && D == A )
+        count++;
+      else {
+         i++;
+           }
+    }
+  }
+  return count;
 };
+// посчитать количество треугольников чисел) там где А имеет значение В, В имеет значение С, С имеет значение А
